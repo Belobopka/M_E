72d1af417dff26ac8196307dd15a7bc659475502
Belobopka <belobaka1@gmail.com>
2020-07-15 20:59:40 +0300

    fixed 2-d task being without random block color

--- 2.js
+++ 2.js
@@ -3,10 +3,29 @@ function script() {
   const boxes = document.getElementById("boxes");
   let width = 30;
   let height = 30;
+
+  const generateRandomColor = () => {
+    var o = Math.round,
+      r = Math.random,
+      s = 255;
+    return (
+      "rgba(" +
+      o(r() * s) +
+      "," +
+      o(r() * s) +
+      "," +
+      o(r() * s) +
+      "," +
+      r().toFixed(1) +
+      ")"
+    );
+  };
+
   const onCreate = () => {
     for (let i = 0; i < input.value; i++) {
       const element = document.createElement("div");
-      element.style.cssText = `width:${width}px; height:${height}px`;
+      randomColor = generateRandomColor();
+      element.style.cssText = `width:${width}px; height:${height}px; background-color:${randomColor}`;
       width = 10 + width;
       height = 10 + width;
       boxes.appendChild(element);
